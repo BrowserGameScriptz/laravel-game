eebdff1794228ab5d6c735965715a6ae94fb3a33
Jaysilendev <jaysilen27@gmail.com>
2018-04-04 10:03:08 +0700

    pingpong resource

---
+++ resources/views/pingpong.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Ping Pong</title>
+</head>
+<body>
+<header>
+<h1>Ping Pong</h1>
+</header>
+<footer>
+This is an example of creating a Ping Pong Game.
+</footer>
+</body>
+<script src="{{ asset('vendor/select2/select2.js') }}"></script>
+<script src="{{ asset('vendor/switchery/dist/switchery.js') }}"></script>
+<script src="{{ asset('vendor/moment/min/moment.min.js') }}"></script>
+<script src="{{ asset('vendor/eonasdan-bootstrap-datetimepicker/src/js/bootstrap-datetimepicker.js') }}"></script>
+<script src="{{ asset('vendor/toastr/toastr.js') }}"></script>
+<script src="{{ asset('js/table/payment_method.js') }}"></script>
+<script>
+$(function(){
+alert("Welcome to the Ping Pong battle.");
+});
+</script>
+</html>
