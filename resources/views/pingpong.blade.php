67693f7768a8d124b1c64639aad5cf9796bee5dc
Jaysilendev <jaysilen27@gmail.com>
2018-04-04 14:41:15 +0700

    add vendor

--- resources/views/pingpong.blade.php
+++ resources/views/pingpong.blade.php
@@ -17,7 +17,26 @@ This is an example of creating a Ping Pong Game.
 <script src="{{ asset('vendor/moment/min/moment.min.js') }}"></script>
 <script src="{{ asset('vendor/eonasdan-bootstrap-datetimepicker/src/js/bootstrap-datetimepicker.js') }}"></script>
 <script src="{{ asset('vendor/toastr/toastr.js') }}"></script>
-<script src="{{ asset('js/table/payment_method.js') }}"></script>
+<script src="{{ asset('vendor/jquery/dist/jquery.min.js') }}"></script>
+<script src="{{ asset('vendor/jquery-ui/jquery-ui.min.js') }}"></script>
+<script src="{{ asset('vendor/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('vendor/pace/pace.min.js') }}"></script>
+<script src="{{ asset('vendor/datatables/media/js/jquery.dataTables.js') }}"></script>
+<script src="{{ asset('vendor/jquery-slimscroll/jquery.slimscroll.min.js') }}"></script>
+<script src="{{ asset('vendor/widgster/widgster.js') }}"></script>
+<script src="{{ asset('vendor/jquery-touchswipe/jquery.touchSwipe.js') }}"></script>
+<script src="{{ asset('vendor/jquery.idle/jquery.idle.min.js') }}"></script>
+<script src="{{ asset('vendor/ladda/dist/spin.min.js') }}"></script>
+<script src="{{ asset('vendor/ladda/dist/ladda.min.js') }}"></script>
+<script src="{{ asset('vendor/ladda/dist/ladda.jquery.min.js') }}"></script>
+<script src="{{ asset('vendor/toastr/toastr.js') }}"></script>
+<script src="{{ asset('vendor/sweetalert/dist/sweetalert.min.js') }}"></script>
+<script src="{{ asset('vendor/bootstrap-select/dist/js/bootstrap-select.min.js') }}"></script>
+
+<script src="{{ asset('js/settings.js') }}"></script>
+<script src="{{ asset('js/app.js') }}"></script>
+<script src="{{ asset('js/form-elements.js') }}"></script>
+<script src="{{ asset('js/dataTables.bootstrap.js') }}"></script>
 <script>
 $(function(){
 alert("Welcome to the Ping Pong battle.");
